a380e4144f0458520ecf0b90daeea6e602daa69a
Ernesto Campohermoso <ecampohermoso@cirrus-it.net>
2023-02-27 18:35:38 -0400

    Subiendo proyecto base

---
+++ src/test/java/bo/edu/ucb/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
